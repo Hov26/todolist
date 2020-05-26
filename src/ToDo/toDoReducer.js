159ce2cea740bfaa28f2f9447d6bf64b9f2f545b
Hov26 <hov.ghevondyan1994@gmail.com>
2020-05-26 17:12:21 +0400

    Install immer and refactor reducers

--- src/ToDo/toDoReducer.js
+++ src/ToDo/toDoReducer.js
@@ -1,3 +1,5 @@
+import produce from "immer";
+
 let initialState = {
   tasksData: [],
   currentValue: "",
@@ -11,86 +13,51 @@ let initialState = {
 };
 
 const toDoReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "UPDATE_INPUT_VALUE": {
-      // const newState = { ...state };
-      // newState.currentValue = action.newTaskMessage;
-      // return newState;
-      return {
-        ...state,
-        currentValue: action.newTaskMessage
-      };
-    }
-    case "ADD_TASK": {
-      const newValue = { task: action.newTask, isDone: false };
-      // return {
-      //   ...state,
-      //   currentValue: "",
-      //   tasksData: [...state.tasksData, newValue]
-      // };
-      const newState = { ...state };
-      newState.tasksData = [...state.tasksData];
-      newState.tasksData.push(newValue);
-      newState.currentValue = "";
-      return newState;
-    }
+  return produce(state, draft => {
+    switch (action.type) {
+      case "UPDATE_INPUT_VALUE": {
+        draft.currentValue = action.newTaskMessage;
+        return;
+      }
+      case "ADD_TASK": {
+        const newValue = { task: action.newTask, isDone: false };
+        draft.tasksData.push(newValue);
+        draft.currentValue = "";
+        return;
+      }
 
-    case "REMOVE_TASK": {
-      // const newState = { ...state };
-      // newState.tasksData.splice(action.idx, 1);
-      // return newState;
+      case "REMOVE_TASK": {
+        draft.tasksData.splice(action.idx, 1);
+        return;
+      }
+      case "SWITCH_TASK_STATE": {
+        const { idx } = action;
+        draft.tasksData[idx].isDone = !draft.tasksData[idx].isDone;
+        return;
+      }
 
-      const newState = { ...state };
-      newState.tasksData = [...state.tasksData];
-      newState.tasksData.splice(action.idx, 1);
-      return newState;
-
-      // const { tasksData } = state;
-      // const { idx } = action;
-      // return {
-      //   ...state,
-      //   tasksData: [
-      //     ...tasksData.slice(0, idx),
-      //     ...tasksData.slice(idx + 1, tasksData.length)
-      //   ]
-      // };
+      case "SHOW_COMPLETED_TASKS": {
+        const newState = { ...state };
+        newState.isCompletedClicked = true;
+        newState.isActiveClicked = false;
+        return newState;
+      }
+      case "SHOW_ACTIVE_TASKS": {
+        const newState = { ...state };
+        newState.isActiveClicked = true;
+        newState.isCompletedClicked = false;
+        return newState;
+      }
+      case "SHOW_ALL_TASKS": {
+        const newState = { ...state };
+        newState.isActiveClicked = false;
+        newState.isCompletedClicked = false;
+        return newState;
+      }
+      default:
+        return state;
     }
-    case "SWITCH_TASK_STATE": {
-      // const newState = { ...state };
-      const { tasksData } = state;
-      const { idx } = action;
-      // newState.tasksData[idx].isDone = !newState.tasksData[idx].isDone;
-      // return newState;
-      return {
-        ...state,
-        tasksData: [
-          ...state.tasksData.slice(0, idx),
-          { ...state.tasksData[idx], isDone: !tasksData[idx].isDone },
-          ...state.tasksData.slice(idx + 1, tasksData.length)
-        ]
-      };
-    }
-    case "SHOW_COMPLETED_TASKS": {
-      const newState = { ...state };
-      newState.isCompletedClicked = true;
-      newState.isActiveClicked = false;
-      return newState;
-    }
-    case "SHOW_ACTIVE_TASKS": {
-      const newState = { ...state };
-      newState.isActiveClicked = true;
-      newState.isCompletedClicked = false;
-      return newState;
-    }
-    case "SHOW_ALL_TASKS": {
-      const newState = { ...state };
-      newState.isActiveClicked = false;
-      newState.isCompletedClicked = false;
-      return newState;
-    }
-    default:
-      return state;
-  }
+  });
 };
 
 export default toDoReducer;
